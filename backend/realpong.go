b7753ef3e05dab1d14876296d2646104d3a6d0b7
Olle Sandberg <olle.sandberg@dinbox.se>
2018-09-17 20:24:32 +0200

    backend: Setup echoing websocekt server.

--- backend/realpong.go
+++ backend/realpong.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 	"os"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func main() {
 	http_port := os.Getenv("HTTP_PORT")
@@ -18,6 +25,32 @@ func main() {
 	log.Printf("Serve static from '" + http_root + "'")
 	fs := http.FileServer(http.Dir(http_root))
 	http.Handle("/", fs)
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println("upgrade: ", err)
+			return
+		}
+		defer conn.Close()
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("read: ", err)
+				return
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Write message back to browser
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				log.Println("write: ", err)
+				return
+			}
+		}
+	})
 
 	log.Printf("Listening for connections at port " + http_port)
 	log.Fatal(http.ListenAndServe(":"+http_port, nil))
