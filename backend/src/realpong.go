1d327f7df6abf29c2d7d5ba7ec3ceee6fb5509e9
Olle Sandberg <olle.sandberg@dinbox.se>
2018-09-17 19:51:06 +0200

    backend: Serve static files from frontend directory.

--- backend/src/realpong.go
+++ backend/src/realpong.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("my realpong app.test.first")
-}
+	http_port := os.Getenv("HTTP_PORT")
+	http_root := os.Getenv("HTTP_ROOT")
+	if len(http_root) == 0 {
+		http_root = "../../frontend"
+	}
+	if len(http_port) == 0 {
+		http_port = "8080"
+	}
+	log.Printf("Serve static from '" + http_root + "'")
+	fs := http.FileServer(http.Dir(http_root))
+	http.Handle("/", fs)
 
+	log.Printf("Listening for connections at port " + http_port)
+	log.Fatal(http.ListenAndServe(":"+http_port, nil))
+}
